07f7e3b500070973fd8f64923c2560226e11bb37
Milena Parysow <48178329@est.ort.edu.ar>
2024-06-28 14:54:02 -0300

    Initial commit

---
+++ Assets/Codigo.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThingsManager : MonoBehaviour
+{
+    public GameObject[] things;
+    public int currentIndex = -1;
+    void Start()
+    {
+        DeactivateAll();
+    }
+
+
+    //jero esto es chat gpt 
+    //borrar esto
+    //
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            currentIndex++;
+            if (currentIndex >= things.Length)
+            {
+                currentIndex = 0;
+            }
+            DeactivateAll();
+            ActivaByIndex(currentIndex);
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            currentIndex--;
+            if (currentIndex <= 0)
+            {
+                currentIndex = things.Length - 1;
+            }
+            DeactivateAll();
+            ActivaByIndex(currentIndex);
+        }
+    }
+
+    void DeactivateAll()
+    {
+        for (int i = 0; i < things.Length; i++)
+        {
+            things[i].SetActive(false);
+        }
+    }
+
+    void ActivaByIndex(int index)
+    {
+        things[index].SetActive(true);
+    }
+}
+
+
+
---
+++ Assets/a.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class a : MonoBehaviour
+{
+    //chat gpt
+    public GameObject[] things;
+    public int currentIndex = -1;
+    void Start()
+    {
+        DeactivateAll();
+    }
+
+
+    // es chat gpt 
+    //borrar esto
+    //
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            currentIndex++;
+            if (currentIndex >= things.Length)
+            {
+                currentIndex = 0;
+            }
+            DeactivateAll();
+            ActivaByIndex(currentIndex);
+        }//chat gpt
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            currentIndex--;
+            if (currentIndex <= 0)
+            {//chat gpt
+                currentIndex = things.Length - 1;
+            }
+            DeactivateAll();
+            ActivaByIndex(currentIndex);
+        }
+    }//chat gpt
+
+    void DeactivateAll()
+    {//chat gpt
+        for (int i = 0; i < things.Length; i++)
+        {//chat gpt
+            things[i].SetActive(false);
+        }
+    }
+    //chat gpt
+    void ActivaByIndex(int index)
+    {
+        things[index].SetActive(true);
+    }
+}
+
+
+
+//chat gpt
